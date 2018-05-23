06de27d37c835599932ed968c00d33660962b0e3
julien <julienroumagnac1@gmail.com>
2018-05-23 10:25:55 +0200

    supp cmp

--- cmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd src
-
-javac fr/battleship/*.java polytech/ig/julien/roumagnac/game/*.java polytech/ig/julien/roumagnac/structure/*.java roumagnac/julien/*.java
-
-java fr.battleship.TestIA
-
-java roumagnac.julien.Battleship
